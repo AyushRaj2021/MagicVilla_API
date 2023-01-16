b9a594e909f038c7ead3c30df5c1176c0d267de8
Ayush <ayushrj1516@gmail.com>
2023-01-16 23:28:10 +0530

    DTO File Steup

---
+++ MagicVilla_VillaAPI/Models/DTO/VillaDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_VillaAPI.Models.DTO
+{
+	public class VillaDTO
+	{
+		//contains only those field which we want to send to endpoint
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
