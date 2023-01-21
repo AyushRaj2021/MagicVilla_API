6dc4c6adb2db6554c2387442aa679ac27141192b
Ayush <ayushrj1516@gmail.com>
2023-01-21 23:42:11 +0530

    connection string

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -3,6 +3,7 @@ using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.DTO;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
@@ -10,16 +11,18 @@ namespace MagicVilla_VillaAPI.Controllers
 	[Route("api/VillaAPI")]
 	[ApiController]
 	public class VillaAPIController : ControllerBase
-	{		
-		public VillaAPIController()
-		{			
+	{
+		private readonly ApplicationDbContext _db;
+		public VillaAPIController(ApplicationDbContext db)
+		{
+			_db = db;
 		}
 
 		[HttpGet]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		public ActionResult<IEnumerable<VillaDTO>> GetVillas()
 		{
- 			return Ok(VillaStore.villaList);
+ 			return Ok(_db.Villas.ToList());
 		}
 
 		[HttpGet("{id:int}", Name = "GetVilla")]
@@ -34,7 +37,7 @@ namespace MagicVilla_VillaAPI.Controllers
 			{
 				return BadRequest();
 			}
-			var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+			var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
 			if (villa == null)
 			{
 				return NotFound();
@@ -52,7 +55,7 @@ namespace MagicVilla_VillaAPI.Controllers
 			//{
 			//    return BadRequest(ModelState);
 			//}
-			if(VillaStore.villaList.FirstOrDefault(u=>u.Name.ToLower() == villaDTO.Name.ToLower())!=null)
+			if(_db.Villas.FirstOrDefault(u=>u.Name.ToLower() == villaDTO.Name.ToLower())!=null)
 			{
 				ModelState.AddModelError("CustomError", "Villa already Exists!");
 				return BadRequest(ModelState);
@@ -65,8 +68,20 @@ namespace MagicVilla_VillaAPI.Controllers
 			{
 				return StatusCode(StatusCodes.Status500InternalServerError);
 			}
-			villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
-			VillaStore.villaList.Add(villaDTO);
+			//we are converting villaDto type to villa type as efcore is of villa type
+			Villa model = new()
+			{
+				Amenity = villaDTO.Amenity,
+				Details = villaDTO.Details,
+				Id = villaDTO.Id,
+				ImageUrl = villaDTO.ImageUrl,
+				Name = villaDTO.Name,
+				Occupancy = villaDTO.Occupancy,
+				Rate = villaDTO.Rate,
+				Sqft = villaDTO.Sqft
+			};
+			_db.Villas.Add(model);
+			_db.SaveChanges();
 			return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO); 
 		}
 
@@ -80,12 +95,12 @@ namespace MagicVilla_VillaAPI.Controllers
 			{
 				return BadRequest();
 			}
-			var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+			var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
 			if (villa == null)
 			{
 				return NotFound();
 			}
-			VillaStore.villaList.Remove(villa);
+			_db.Villas.Remove(villa);
 			return NoContent();
 		}
 
@@ -98,11 +113,25 @@ namespace MagicVilla_VillaAPI.Controllers
 			{
 				return BadRequest();
 			}
-			var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
-			villa.Name = villaDTO.Name;
-			villa.Sqft = villaDTO.Sqft;
-			villa.Occupancy = villaDTO.Occupancy;
+			//var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+			//villa.Name = villaDTO.Name;
+			//villa.Sqft = villaDTO.Sqft;
+			//villa.Occupancy = villaDTO.Occupancy;
 
+			//we are converting villaDto type to villa type as ef core is of villa type
+			Villa model = new()
+			{
+				Amenity = villaDTO.Amenity,
+				Details = villaDTO.Details,
+				Id = villaDTO.Id,
+				ImageUrl = villaDTO.ImageUrl,
+				Name = villaDTO.Name,
+				Occupancy = villaDTO.Occupancy,
+				Rate = villaDTO.Rate,
+				Sqft = villaDTO.Sqft
+			};
+			_db.Villas.Update(model);
+			_db.SaveChanges();
 			return NoContent();
 
 		}
@@ -116,12 +145,37 @@ namespace MagicVilla_VillaAPI.Controllers
 			{
 				return BadRequest();
 			}
-			var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+			var villa = _db.Villas.AsNoTracking().FirstOrDefault(u => u.Id == id);
+			//we are converting villa type to villadto type as patch object is of villadto type
+			VillaDTO villaDTO = new()
+			{
+				Amenity = villa.Amenity,
+				Details = villa.Details,
+				Id = villa.Id,
+				ImageUrl = villa.ImageUrl,
+				Name = villa.Name,
+				Occupancy = villa.Occupancy,
+				Rate = villa.Rate,
+				Sqft = villa.Sqft
+			};			
 			if(villa == null)
 			{
 				return BadRequest();
 			}
-			patchDTO.ApplyTo(villa, ModelState);
+			patchDTO.ApplyTo(villaDTO, ModelState);
+			Villa model = new Villa()
+			{
+				Amenity = villaDTO.Amenity,
+				Details = villaDTO.Details,
+				Id = villaDTO.Id,
+				ImageUrl = villaDTO.ImageUrl,
+				Name = villaDTO.Name,
+				Occupancy = villaDTO.Occupancy,
+				Rate = villaDTO.Rate,
+				Sqft = villaDTO.Sqft
+			};
+			_db.Villas.Update(model);
+			_db.SaveChanges();
 			if (!ModelState.IsValid)
 			{
 				return BadRequest(ModelState);
